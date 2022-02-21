26c5421b07c48971c5ab94e3db3ea950f4d70968
Tanja Rannikko <tanja.rannikko@tuni.fi>
2022-02-21 14:13:39 +0200

    index.js: Add variables for position and nextMark.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var readlineSync = require("readline-sync");
 var player1 = "Player 1";
 var player2 = "Player 2";
 
+// position tells where the player wants to play.
+var position = "0";
+// nextMark is either "X" or "0". It tells which mark will be placed
+// to the board next. It can also tells whose turn it is.
+var nextMark = "X";
+
 //These can be used, if we want to ask the names for the players.
 //player1 = readlineSync.question("Give the name for player 1: ");
 //player2 = readlineSync.question("Give the name for player 1: ");
