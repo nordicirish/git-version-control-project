c596d1a72c08fbb9ec215bff5e5f75e4c45d5ce3
Tanja Rannikko <tanja.rannikko@tuni.fi>
2022-02-21 14:04:46 +0200

    index.js: Require readline-sync and add variables for the players.

---
+++ index.js
@@ -0,0 +1,10 @@
+var readlineSync = require("readline-sync");
+
+var player1 = "Player 1";
+var player2 = "Player 2";
+
+//These can be used, if we want to ask the names for the players.
+//player1 = readlineSync.question("Give the name for player 1: ");
+//player2 = readlineSync.question("Give the name for player 1: ");
+
+console.log(player1 + " and " + player2 + ", welcome to play tic-tac-toe!");
