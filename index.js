c09934a6659bf9d427ee6a8ebb77b4e7650c5ee8
Tanja Rannikko <tanja.rannikko@tuni.fi>
2022-03-04 13:52:22 +0200

    index.js: Add variable for a board.

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ var player2 = "Player 2";
 // position tells where the player wants to play.
 var position = "0";
 // nextMark is either "X" or "0". It tells which mark will be placed
-// to the board next. It can also tells whose turn it is.
+// to the board next. It can also tell whose turn it is.
 var nextMark = "X";
+// The board is an array which consists of three arrays (=rows).
+// Each inner array has three elements - one for each column.
+var board = [
+  [" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "],
+];
 
 //These can be used, if we want to ask the names for the players.
 //player1 = readlineSync.question("Give the name for player 1: ");
